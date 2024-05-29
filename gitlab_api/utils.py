dd8a4e8830b4ea07e275a76c4a7f7505593f6382
knucklessg1 <knucklessg1@gmail.com>
2024-05-28 22:08:30 -0500

    Added utils logging.

--- gitlab_api/utils.py
+++ gitlab_api/utils.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python
 # coding: utf-8
+import logging
 from typing import Union
 
 import requests
 
 try:
-    from servicenow_api.servicenow_models import (
+    from gitlab_api.gitlab_models import (
         Response,
     )
 except ModuleNotFoundError:
-    from servicenow_models import (
+    from gitlab_models import (
         Response,
     )
+logging.basicConfig(
+    level=logging.ERROR, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 def process_response(response: requests.Response) -> Union[Response, requests.Response]:
     try:
         response.raise_for_status()
     except Exception as response_error:
-        print(f"Response Error: {response_error}")
+        logging.error(f"Response Error: {response_error}")
     status_code = response.status_code
     raw_output = response.content
     try:
         response = response.json()
     except Exception as response_error:
-        print(f"JSON Conversion Error: {response_error}")
+        logging.error(f"JSON Conversion Error: {response_error}")
     try:
         response = Response(
             **response,
@@ -33,6 +37,6 @@ def process_response(response: requests.Response) -> Union[Response, requests.Re
             json_output=response,
         )
     except Exception as response_error:
-        print(f"Response Model Application Error: {response_error}")
+        logging.error(f"Response Model Application Error: {response_error}")
 
     return response
