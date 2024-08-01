a6a0abeea932f28f314625c5610ede74846161df
KnucklesSG1 <knucklessg1@gmail.com>
2024-07-31 21:39:00 -0500

    Checking in

--- gitlab_api/utils.py
+++ gitlab_api/utils.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 import logging
 from typing import Union
-
+from sqlalchemy.orm import class_mapper
 import requests
 
 try:
@@ -12,6 +12,15 @@ try:
 except ModuleNotFoundError:
     from gitlab_models import (
         Response,
+    )
+
+try:
+    from gitlab_api.gitlab_db_models import (
+        LabelsDBModel, LabelDBModel,
+)
+except ModuleNotFoundError:
+    from gitlab_db_models import (
+        LabelsDBModel, LabelDBModel,
     )
 logging.basicConfig(
     level=logging.ERROR, format="%(asctime)s - %(levelname)s - %(message)s"
@@ -48,6 +57,39 @@ def remove_none_values(d: dict) -> dict:
     return {k: v for k, v in d.items() if v is not None}
 
 
+def get_related_model(sqlalchemy_model, key, value):
+    """Get the related SQLAlchemy model for a given key."""
+    # Handle special cases explicitly
+    if key == 'labels':
+        label_entries = value.get('labels', [])
+
+        label_instances = []
+        for label_entry in label_entries:
+            label_instance = LabelDBModel(
+                name=label_entry['name'],
+                color=label_entry.get('color', 'default_color'),  # Default color if not provided
+                text_color=label_entry.get('text_color', 'default_text_color'),  # Default text color if not provided
+                description=label_entry.get('description', ''),
+                description_html=label_entry.get('description_html', ''),
+                open_issues_count=label_entry.get('open_issues_count', 0),
+                closed_issues_count=label_entry.get('closed_issues_count', 0),
+                open_merge_requests_count=label_entry.get('open_merge_requests_count', 0),
+                subscribed=label_entry.get('subscribed', False),
+                priority=label_entry.get('priority', None),
+                is_project_label=label_entry.get('is_project_label', True)
+            )
+            label_instances.append(label_instance)
+            labels_collection = LabelsDBModel(base_type="Labels", labels=label_instances)
+        return LabelsDBModel
+
+    # Dynamically determine the related model based on the attribute
+    mapper = class_mapper(sqlalchemy_model)
+    for prop in mapper.iterate_properties:
+        if prop.key == key:
+            return prop.mapper.class_
+    raise ValueError(f"Unable to find related model for key: {key}")
+
+
 def pydantic_to_sqlalchemy(pydantic_model):
     # Check if the model is already converted by ensuring the model doesn't have Meta pydantic field,
     # but does have base_type sqlalchemy field.
@@ -56,12 +98,14 @@ def pydantic_to_sqlalchemy(pydantic_model):
         or not hasattr(pydantic_model.Meta, "orm_model")
     ) and hasattr(pydantic_model, "base_type"):
         sqlalchemy_instance = pydantic_model
+        print(f"\n\nFound SQLAlchemy Model on First Try: {sqlalchemy_instance}")
         return sqlalchemy_instance
     sqlalchemy_model = pydantic_model.Meta.orm_model
     sqlalchemy_instance = sqlalchemy_model()
     for key, value in pydantic_model.model_dump(exclude_unset=True).items():
         if isinstance(value, list):
             if len(value) > 0:
+                print(f"\n\nValue that is a list: {value} for key: {key}")
                 related_instances = [
                     (
                         pydantic_to_sqlalchemy(item)
@@ -70,16 +114,24 @@ def pydantic_to_sqlalchemy(pydantic_model):
                     )
                     for item in value
                 ]
+                print(f"\n\nRelated instances: {related_instances}")
                 setattr(sqlalchemy_instance, key, related_instances)
         elif isinstance(value, dict):
-            related_sqlalchemy_model = getattr(
-                sqlalchemy_model, key
-            ).property.mapper.class_
+            print(f"\n\nValue that is a dict: {value} for key: {key}")
+            related_sqlalchemy_model = get_related_model(sqlalchemy_model, key, value)
+            print(f"\n\nRelated instance: {related_sqlalchemy_model}")
             value = remove_none_values(value)
             nested_model = related_sqlalchemy_model(**value)
+            print(f"\n\nObtained Nested Model: {nested_model}")
             related_instance = pydantic_to_sqlalchemy(nested_model)
+            print(f"\n\nDefined SQLAlchemy: {related_instance}")
             setattr(sqlalchemy_instance, key, related_instance)
+            print(f"\n\nSQLAlchemy Model Set: {related_instance}")
         else:
             if value:
+                print(f"\n\nImmediately Setting Attribute: {value}")
                 setattr(sqlalchemy_instance, key, value)
+                print(f"\n\nImmeidately Set Attribute: {value}")
+
+    print(f"\n\nCompleted Conversion for: {sqlalchemy_instance}")
     return sqlalchemy_instance
