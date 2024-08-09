9f85c741c78bd0cc56cae2851008ec8013f3b602
KnucklesSG1 <knucklessg1@gmail.com>
2024-08-08 21:46:59 -0500

    Commiting cleaned up comments

--- gitlab_api/utils.py
+++ gitlab_api/utils.py
@@ -271,23 +271,6 @@ def upsert_row(session, db_model, processed_models=None):
     # Upsert nested models first
     upsert_nested_models(session, db_model)
 
-    # # Now handle the main model
-    # existing_model = session.query(model_type).filter_by(id=db_model.id).first()
-    # if existing_model:
-    #     logging.debug(f"\n\nFound Existing Model: {existing_model}")
-    #     try:
-    #         for attr, value in db_model.__dict__.items():
-    #             if attr != "_sa_instance_state":
-    #                 setattr(existing_model, attr, value)
-    #     except Exception as e:
-    #         logging.debug(f"Unable to merge: {e}")
-    #     logging.debug(f"Merged {model_type.__name__} with ID {existing_model.id}")
-    # else:
-    #     logging.debug(f"\nSetting Existing Model: {db_model}")
-    #     existing_model = db_model
-    #     session.add(existing_model)
-    #     logging.debug(f"\nInserted new {model_type.__name__} with ID {existing_model.id}")
-
     try:
         existing_model = session.merge(db_model)
         session.commit()
@@ -299,53 +282,6 @@ def upsert_row(session, db_model, processed_models=None):
         )
 
     return existing_model
-
-
-# def upsert_row(session, db_model, processed_models=None):
-#     if db_model is None:
-#         return None
-#
-#     if processed_models is None:
-#         processed_models = set()
-#
-#     model_type = type(db_model)
-#     model_identifier = (model_type, db_model.id)
-#     logging.debug(f"Searching for {db_model.id} in {model_type}")
-#
-#     if model_identifier in processed_models:
-#         return None
-#
-#     processed_models.add(model_identifier)
-#
-#     # Function to upsert nested models
-#     def upsert_nested_models(session, db_model):
-#         related_models = []
-#         for relation in db_model.__mapper__.relationships:
-#             related_model = getattr(db_model, relation.key)
-#             if isinstance(related_model, InstrumentedList):
-#                 for item in related_model:
-#                     if item is not None:
-#                         related_models.append(item)
-#             elif related_model is not None:
-#                 related_models.append(related_model)
-#
-#         # Recursively upsert nested models first
-#         for related_model in related_models:
-#             upsert_row(session=session, db_model=related_model, processed_models=processed_models)
-#
-#     # Upsert nested models first
-#     upsert_nested_models(session, db_model)
-#
-#     # Now handle the main model using merge
-#     try:
-#         existing_model = session.merge(db_model)
-#         session.commit()
-#         logging.debug(f"Upserted {model_type.__name__} with ID {existing_model.id}")
-#     except Exception as e:
-#         session.rollback()
-#         logging.debug(f"Error inserting/updating {model_type.__name__} with ID {db_model.id}: {e}")
-#
-#     return existing_model
 
 
 def create_table(db_instance, engine):
