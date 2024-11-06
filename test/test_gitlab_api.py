f9118dde9d2f52d8f978a6fd04edf212d435df4f
KnucklesSG1 <knucklessg1@gmail.com>
2024-11-06 16:06:52 -0600

    Fixed verbose output.

--- test/test_gitlab_api.py
+++ test/test_gitlab_api.py
@@ -24,63 +24,63 @@ token = os.environ.get("GITLAB_TOKEN", default="NA")
 client = gitlab_api.Api(url=gitlab_url, token=token, verify=False)
 
 
-# @pytest.mark.skipif(
-#     sys.platform in ["darwin"] or skip,
-#     reason=reason,
-# )
-# def test_get_nested_projects():
-#     # Get nested projects
-#     group_id = 2
-#     projects = client.get_nested_projects_by_group(group_id=group_id, per_page=3)
-#     assert len(projects.data) > 0
-#     assert isinstance(projects.data, list)
-#     group_id = 6
-#     projects = client.get_nested_projects_by_group(group_id=group_id, per_page=3)
-#     assert len(projects.data) > 0
-#     assert isinstance(projects.data, list)
-#
-#
-# @pytest.mark.skipif(
-#     sys.platform in ["darwin"] or skip,
-#     reason=reason,
-# )
-# def test_create_branch():
-#     # Create branch
-#     project = 79
-#     response = client.create_branch(
-#         project_id=project, branch="test_branch", reference="main"
-#     )
-#     assert response.status_code == 201 or response.status_code == 400
-#
-#
-# @pytest.mark.skipif(
-#     sys.platform in ["darwin"] or skip,
-#     reason=reason,
-# )
-# def test_create_project_rule():
-#     # Create branch
-#     project = 79
-#     response = client.create_project_level_rule(
-#         project_id=project, name="Test_Rule", approvals_required=9
-#     )
-#     assert response.status_code == 201
-#
-#
-# @pytest.mark.skipif(
-#     sys.platform in ["darwin"] or skip,
-#     reason=reason,
-# )
-# def test_get_project_rules():
-#     # Create branch
-#     project = 79
-#     response = client.get_project_level_rules(project_id=project)
-#     assert response.status_code == 200
+@pytest.mark.skipif(
+    sys.platform in ["darwin"] or skip,
+    reason=reason,
+)
+def test_get_nested_projects():
+    # Get nested projects
+    group_id = 2
+    projects = client.get_nested_projects_by_group(group_id=group_id, per_page=3)
+    assert len(projects.data) > 0
+    assert isinstance(projects.data, list)
+    group_id = 6
+    projects = client.get_nested_projects_by_group(group_id=group_id, per_page=3)
+    assert len(projects.data) > 0
+    assert isinstance(projects.data, list)
 
 
 @pytest.mark.skipif(
     sys.platform in ["darwin"] or skip,
     reason=reason,
+)
+def test_create_branch():
+    # Create branch
+    project = 79
+    response = client.create_branch(
+        project_id=project, branch="test_branch", reference="main"
     )
+    assert response.status_code == 201 or response.status_code == 400
+
+
+@pytest.mark.skipif(
+    sys.platform in ["darwin"] or skip,
+    reason=reason,
+)
+def test_create_project_rule():
+    # Create branch
+    project = 79
+    response = client.create_project_level_rule(
+        project_id=project, name="Test_Rule", approvals_required=9
+    )
+    assert response.status_code == 201
+
+
+@pytest.mark.skipif(
+    sys.platform in ["darwin"] or skip,
+    reason=reason,
+)
+def test_get_project_rules():
+    # Create branch
+    project = 79
+    response = client.get_project_level_rules(project_id=project)
+    assert response.status_code == 200
+
+
+@pytest.mark.skipif(
+    sys.platform in ["darwin"] or skip,
+    reason=reason,
+)
 def test_edit_group():
     # Get nested projects
     group_id = 6
@@ -89,8 +89,8 @@ def test_edit_group():
 
 
 if __name__ == "__main__":
-    # test_get_nested_projects()
-    # test_create_branch()
-    # test_create_project_rule()
-    # test_get_project_rules()
+    test_get_nested_projects()
+    test_create_branch()
+    test_create_project_rule()
+    test_get_project_rules()
     test_edit_group()
