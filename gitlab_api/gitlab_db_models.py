b11f7cd0be756877de54dbc8484bfbec68aa085d
KnucklesSG1 <knucklessg1@gmail.com>
2025-02-11 19:25:29 -0600

    Fixed comments.

--- gitlab_api/gitlab_db_models.py
+++ gitlab_api/gitlab_db_models.py
@@ -633,7 +633,7 @@ class AccessLevelDBModel(BaseDBModel):
     group: Mapped["GroupDBModel"] = relationship(
         back_populates="access_levels",
     )
-    
+    # Specify the foreign keys for each relationship
     branches_push_access: Mapped[List["BranchDBModel"]] = relationship(
         back_populates="push_access_levels",
         foreign_keys="[BranchDBModel.push_access_levels_id]",
