3073fb9f042b726f990d1fab7f7aa58a966675a3
KnucklesSG1 <knucklessg1@gmail.com>
2025-02-13 11:24:27 -0600

    Fixed entity relationships for package and project

--- test/test_gitlab_db_models.py
+++ test/test_gitlab_db_models.py
@@ -418,11 +418,11 @@ from gitlab_api.gitlab_db_models import (
                 "name": "project_1",
                 "created_at": datetime.now(),
                 "creator": UserDBModel(**{"id": 1, "username": "creator"}),
-                "topics": [
-                    TopicDBModel(**{"name": "Test Topic 1"}),
-                    TopicDBModel(**{"name": "Test Topic 2"}),
-                    TopicDBModel(**{"name": "Test Topic 3"}),
-                ],
+                # "topics": [
+                #     TopicDBModel(**{"name": "Test Topic 1"}),
+                #     TopicDBModel(**{"name": "Test Topic 2"}),
+                #     TopicDBModel(**{"name": "Test Topic 3"}),
+                # ],
                 "shared_with_groups": [
                     GroupDBModel(
                         **{
