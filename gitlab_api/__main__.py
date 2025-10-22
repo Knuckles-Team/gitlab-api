949a82e55aeef4aff1e87f30d0fac819790940ce
KnucklesSG1 <knucklessg1@gmail.com>
2025-10-21 22:20:58 -0500

    Fixed main.py

--- gitlab_api/__main__.py
+++ gitlab_api/__main__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # coding: utf-8
 
-from gitlab_api.gitlab_api_mcp import gitlab_api_mcp
+from gitlab_api.gitlab_mcp import gitlab_mcp
 
 if __name__ == "__main__":
-    gitlab_api_mcp()
+    gitlab_mcp()
